3487f17cff8ea6d3576333f3596b5165c6aa8e18
Oggy1907 <oguzhangonel@hotmail.com>
2015-06-21 18:57:41 -0400

    I added it so that we have a score and level.

--- ArcadeGame1/src/arcadegame1package1/HUD.java
+++ ArcadeGame1/src/arcadegame1package1/HUD.java
@@ -8,6 +8,8 @@ public class HUD {
 	public static int Health = 100;
 	
 	private int greenValue = 255;
+	private int score = 0;
+	private int level = 1;
 	
 	public void tick(){
 		Health = GameMain.clamp(Health, 0, 100);
@@ -23,5 +25,7 @@ public class HUD {
 		g.fillRect(15, 15, Health * 2, 32);
 		g.setColor(Color.white);
 		g.drawRect(15, 15, 200, 32);
+		g.drawString("Score: " + score, 15, 64);
+		g.drawString("Level: " + level, 15, 80);
 	}
 }
